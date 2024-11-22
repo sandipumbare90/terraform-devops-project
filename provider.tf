cf6e432a2ed9d4f79a5517996d06d21966f48682
Ubuntu <ubuntu@jenkins-master-vm-instance.asia-south2-b.c.myfirstdevopsproject-442307.internal>
2024-11-22 07:18:09 +0000

    terraform provider details added

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "6.0.0"
+    }
+  }
+}
+
 provider "google" {
   project = "myfirstdevopsproject-442307"
   region = "asia-south1"
