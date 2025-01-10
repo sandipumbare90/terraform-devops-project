96af87d67b2bdad165c3538ff6772a268dd4fc25
sandipumbare90 <160105096+sandipumbare90@users.noreply.github.com>
2025-01-10 17:43:38 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "google" {
   project = "myfirstdevopsproject-442307"
-  region = "asia-south2"
-  impersonate_service_account = "main-sa-having-actual-roles@myfirstdevopsproject-442307.iam.gserviceaccount.com"
+  region = "europe-west4"
+  #impersonate_service_account = "main-sa-having-actual-roles@myfirstdevopsproject-442307.iam.gserviceaccount.com"
 }
