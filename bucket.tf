6020c853c51eeaa0d8a164030fbc0e5b9ba8e937
sandipumbare90 <160105096+sandipumbare90@users.noreply.github.com>
2025-01-20 13:00:15 +0530

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -1,4 +1,4 @@
-resource "google_storage_bucket" "my-bucket-for-github" {
+resource "google_storage_bucket" "my-bucket-for-github-01" {
   name = "my-bucket-for-github"
   location = "europe-west4"
 }
