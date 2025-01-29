dedeb9d2898237c101351eca201ec2f4300e09be
sandipumbare90 <160105096+sandipumbare90@users.noreply.github.com>
2025-01-29 17:43:52 +0530

    Updated bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -1,5 +1,7 @@
 resource "google_storage_bucket" "my-bucket-for-github-01" {
   name = "my-bucket-for-github"
   location = "europe-west4"
-  uniform_bucket_level_access = true
+   versioning {
+    enabled = true
+  }
 }
