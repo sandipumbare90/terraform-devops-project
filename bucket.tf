9502724f70e75647e75a42c01225d95d9f465646
sandipumbare90 <160105096+sandipumbare90@users.noreply.github.com>
2025-01-29 18:21:48 +0530

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -4,4 +4,12 @@ resource "google_storage_bucket" "my-bucket-for-github-01" {
   versioning {
     enabled = true
   }
+  lifecycle_rule {
+    condition {
+      age = 30 # Delete objects older than 30 days
+    }
+    action {
+      type = "Delete"
+    }
+  }
 }
